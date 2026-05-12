c91d847e6719240debd64ec25a28b97b62d7a551
Isabela Rocha Do Nascimento <isabela.rochadonascimento@gmail.com>
2026-05-12 13:00:38 -0300

    Create interface.h

---
+++ PEM-20261-ATIVIDADES/PEM-20261-Semana09/interface.h
@@ -0,0 +1,16 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+// mostra o menu
+void mostrarMenu();
+
+// executa opcao decimal para binario
+void menuBinario();
+
+// executa opcao potencia
+void menuPotencia();
+
+// executa opcao soma de digitos
+void menuSomaDigitos();
+
+#endif
