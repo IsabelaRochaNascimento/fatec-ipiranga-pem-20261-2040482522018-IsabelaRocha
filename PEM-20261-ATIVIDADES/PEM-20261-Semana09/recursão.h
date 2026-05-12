d7272fd9ff308e0e1009596fc6ebb0d3ccdd9abe
Isabela Rocha Do Nascimento <isabela.rochadonascimento@gmail.com>
2026-05-12 12:57:00 -0300

    Create recursão.h

---
+++ PEM-20261-ATIVIDADES/PEM-20261-Semana09/recursão.h
@@ -0,0 +1,13 @@
+#ifndef RECURSAO_H
+#define RECURSAO_H
+
+// converte decimal para binario
+void decToBin(int n);
+
+// calcula potencia
+int potencia(int base, int exp);
+
+// soma os digitos de um numero
+int somaDigitos(int n);
+
+#endif
